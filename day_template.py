2d8f6b4d8110e4f593315b9b2cc55b4589ac4eb0
andreas-becker-98 <andreasb.1811@gmail.com>
2020-10-19 14:15:35 +0100

    Added a day template script.

---
+++ day_template.py
@@ -0,0 +1,10 @@
+# Copyright 2020 by Andreas Becker
+
+welcome_text = "Advent of Code, Day X:"
+farewell_text = "That's all folks!"
+
+print(welcome_text)
+
+# Add actual functionality here.
+
+print(farewell_text)
