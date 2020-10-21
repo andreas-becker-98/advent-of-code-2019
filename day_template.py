7750b769c721d88c9757044beec4adcf4f0fb126
andreas-becker-98 <andreasb.1811@gmail.com>
2020-10-21 18:40:10 +0100

    Changed 'day_template.py' to reflect structure introduced in 'day_one.py'

--- day_template.py
+++ day_template.py
@@ -1,10 +1,15 @@
 # Copyright 2020 by Andreas Becker
 
+# Global variables
 welcome_text = "Advent of Code, Day X:"
 farewell_text = "That's all folks!"
 
+# Functions
+
+
+# Main body
 print(welcome_text)
 
-# Add actual functionality here.
+	# Add actual functionality here.
 
 print(farewell_text)
